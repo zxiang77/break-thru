898821110427efee265dfec25763f65521b6b464
spcb <spcb@users.noreply.bitbucket.org>
2017-04-22 11:24:14 -0400

    Add trade-off analytics API config

--- server.js
+++ server.js
@@ -2,7 +2,52 @@ var http = require('http');
 var fs = require('fs');
 
 const PORT=3005;
+//---------------------------------------
+/* TRADE-OFF ANALYTICS */
+// {
+// 	  "url": "https://gateway.watsonplatform.net/tradeoff-analytics/api",
+// 		  "username": "0190f0c0-f15a-4878-8728-d88b57bd326f",
+// 		  "password": "tNPahu8wlMct"
+// }
 
+var username = '0190f0c0-f15a-4878-8728-d88b57bd326f';
+var password = 'tNPahu8wlMct';
+
+
+var TradeoffAnalyticsV1 = require('watson-developer-cloud/tradeoff-analytics/v1');
+var tradeoff_analytics = new TradeoffAnalyticsV1({
+	  username: username,
+	  password: password
+});
+
+
+var TradeoffAnalyticsV1 = require('watson-developer-cloud/tradeoff-analytics/v1');
+var tradeoff_analytics = new TradeoffAnalyticsV1({
+	  username: username,
+	  password: password,
+	  headers: {
+		      'X-Watson-Learning-Opt-Out': 'true'
+		    }
+});
+
+
+var TradeoffAnalyticsV1 = require('watson-developer-cloud/tradeoff-analytics/v1');
+var tradeoff_analytics = new TradeoffAnalyticsV1({
+  username: username,
+  password: password
+});
+
+// Parameters for the call are defined in the problem.json file.
+var params = require('problem.json');
+
+tradeoff_analytics.dilemmas(params, function(error, resolution) {
+  if (error)
+    console.log('Error:', error)
+  else
+    console.log(JSON.stringify(resolution, null, 2));
+});
+
+//---------------------------------------
 fs.readFile('./index.html', function (err, html) {
 
     if (err) throw err;
@@ -13,3 +58,4 @@ fs.readFile('./index.html', function (err, html) {
         response.end();
     }).listen(PORT);
 });
+//-----------------
