d1151239776de02a9ae04fc1133aaf95d022e260
Zili Xiang <zx77@cornell.edu>
2017-04-22 15:49:18 -0400

    display data on page

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 var express = require('express');
 var app = express();
 
+// connect to Watson tradeoff-analytics
 
 var username = '0190f0c0-f15a-4878-8728-d88b57bd326f';
 var password = 'tNPahu8wlMct';
@@ -32,17 +33,36 @@ var tradeoff_analytics = new TradeoffAnalyticsV1({
     password: password
 });
 
+// connecting to mongodb
+
+var MongoClient = require('mongodb').MongoClient
+    , assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/myproject';
+var data = {test : 123};
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+
+    db.close();
+});
+
 // Parameters for the call are defined in the problem.json file.
 var params = require('problem.json');
 
-tradeoff_analytics.dilemmas(params, function(error, resolution) {
+data = tradeoff_analytics.dilemmas(params, function(error, resolution) {
     if (error)
         console.log('Error:', error)
-    else
+    else {
         console.log(JSON.stringify(resolution, null, 2));
+        data = JSON.stringify(resolution, null, 2);
+        data.input = resolution;
+        return resolution;
+        }
 });
-
-
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -53,7 +73,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-    response.render('pages/index');
+    response.render('pages/index', data);
 });
 
 app.listen(app.get('port'), function() {
